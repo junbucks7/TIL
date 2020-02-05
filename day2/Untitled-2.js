1a6a199f12f00c01cc7df004b188de0c4f797a16
junbucks7 <juney214@gmail.com>
2020-02-05 11:03:03 +0900

    시험 성적

---
+++ day2/Untitled-2.js
@@ -0,0 +1,13 @@
+const gra = 90;
+
+if (gra >= 90) {
+  console.log("A");
+} else if (gra >= 80 && gra < 90) {
+  console.log("B");
+} else if (gra >= 70 && gra < 80) {
+  console.log("C");
+} else if (gra >= 60 && gra < 70) {
+  console.log("D");
+} else {
+  console.log("F");
+}
